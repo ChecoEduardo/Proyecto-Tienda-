c319b7dd0b414dfaddf17b646b4e936da3c81284
Checo <sergio_eduardosandoval@outlook.es>
2018-04-29 19:19:50 -0500

    se creo un arreglo vacio para posteriormente llenarlo con todos los que cumplan con una popularidad de 5

--- Js/InicioCtrl.js
+++ Js/InicioCtrl.js
@@ -1,6 +1,11 @@
 app.controller('inicioCtrl',['$scope','Lista' ,function($scope,Lista ){
-
   $scope.ListadoItem=Lista;
-  
+  $scope.ArregloPopular=[];
+
+
+for(i=0; i<$scope.ListadoItem.length; i++){
+      if($scope.ListadoItem[i].Popular==5){
+        $scope.ArregloPopular.push($scope.ListadoItem[i])
+      }}
 
 }]);
