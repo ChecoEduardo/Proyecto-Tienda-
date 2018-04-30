a992f25ffce2c43bd40115e9c5a6e4e60a0aadf7
Checo <sergio_eduardosandoval@outlook.es>
2018-04-30 10:54:35 -0500

    Se creo la funcionalidad de buscar un producto

--- Js/BusquedaCtrl.js
+++ Js/BusquedaCtrl.js
@@ -1,3 +1,10 @@
-app.controller('busqueCtrl',['$scope','$routeParams' ,function($scope,$routeParams){
+app.controller('busqueCtrl',['$scope','$routeParams','Lista' ,function($scope,$routeParams,Lista){
+  $scope.ListaItems=Lista;
+  $scope.itemsSel={};
 
+  $scope.search=$routeParams.Buscar;
+  $scope.AbrirModal = function (items){
+       angular.copy(items,$scope.itemsSel);
+       $("#modal_item").modal();
+   }
 }]);
