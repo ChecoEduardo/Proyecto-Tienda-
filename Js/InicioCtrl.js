9164ddf6230e1704136ccbb69893e4b676f7a6c9
Checo <sergio_eduardosandoval@outlook.es>
2018-04-30 17:11:31 -0500

    Se agrego un filtro en inicio y algunas cosas de css

--- Js/InicioCtrl.js
+++ Js/InicioCtrl.js
@@ -3,10 +3,44 @@ app.controller('inicioCtrl',['$scope','Lista' ,function($scope,Lista ){
   $scope.ArregloPopular=[];
     $scope.itemsSel={};
 
+
+    $scope.MenorMil=[];
+    $scope.menor3mil=[];
+    $scope.Menor5mil=[];
+    $scope.Mayor5mil=[];
+
+
+
 for(i=0; i<$scope.ListadoItem.length; i++){
       if($scope.ListadoItem[i].Popular==5){
         $scope.ArregloPopular.push($scope.ListadoItem[i])
       }}
+
+
+for(i=0; i<$scope.ListadoItem.length; i++){
+    if($scope.ListadoItem[i].Precio<1000){
+      $scope.MenorMil.push($scope.ListadoItem[i])
+    }
+    if($scope.ListadoItem[i].Precio>1000 && $scope.ListadoItem[i].Precio<3000){
+      $scope.menor3mil.push($scope.ListadoItem[i])
+    }
+    if($scope.ListadoItem[i].Precio>3000 && $scope.ListadoItem[i].Precio<5000){
+      $scope.Menor5mil.push($scope.ListadoItem[i])
+    }
+    if($scope.ListadoItem[i].Precio>5000){
+      $scope.Mayor5mil.push($scope.ListadoItem[i])
+    }
+}
+console.log($scope.MenorMil.length*100/36);
+console.log($scope.menor3mil.length*100/36);
+console.log($scope.Menor5mil.length*100/36);
+console.log($scope.Mayor5mil.length*100/36);
+
+
+
+
+
+
 
 $scope.AbrirModal = function (items){
       angular.copy(items,$scope.itemsSel);
